20a9f43010a57376105d5d0c180704abdc2a1426
ivandomanevsky <ivandomanevsky@gmail.com>
2024-03-22 23:11:07 +0300

    add mail sender

---
+++ mail/sender_test.go
@@ -0,0 +1,29 @@
+package mail
+
+import (
+	"github.com/ivandomanevsky/bank-app/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+	<h1>Hello world</h1>
+	<p>This is a test message</p>
+	`
+	to := []string{"test@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
