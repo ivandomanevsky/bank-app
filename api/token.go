22741ad57439ebc3ff911c037364d7840ba8f118
ivandomanevsky <ivandomanevsky@gmail.com>
2024-03-23 00:48:20 +0300

    add Role to CreateToken

--- api/token.go
+++ api/token.go
@@ -1,9 +1,10 @@
 package api
 
 import (
-	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	db "github.com/ivandomanevsky/bank-app/db/sqlc"
 	"net/http"
 	"time"
 )
@@ -32,7 +33,7 @@ func (server *Server) renewAccessToken(ctx *gin.Context) {
 
 	session, err := server.store.GetSession(ctx, refreshPayload.ID)
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if errors.Is(err, db.ErrRecordNotFound) {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
 			return
 		}
@@ -66,6 +67,7 @@ func (server *Server) renewAccessToken(ctx *gin.Context) {
 
 	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
 		refreshPayload.Username,
+		refreshPayload.Role,
 		server.config.AccessTokenDuration,
 	)
 	if err != nil {
